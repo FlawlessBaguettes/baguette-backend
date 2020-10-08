b7d6c7dfe5fdcec43594db019c969fa313948ae5
shusnain <sibgatulhusnain@gmail.com>
2020-10-08 00:29:46 -0400

    Load settings.py when app runs

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+import settings
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.getenv('APP_SETTINGS'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from baguette_backend.models import content, post, user
-import user
-# import email_address
-import post
-import content
+# import user
+# # import email_address
+# import post
+# import content
 
 @app.route('/')
 def index():
