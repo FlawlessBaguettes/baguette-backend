863d667fb49e35533255eb79ffe0b2c8ac6c3537
Raghav Goswami <raghav.goswami576@gmail.com>
2021-02-21 18:56:21 -0500

    fix syntax error add auth route add functionality for session tokens and hashed passwords

--- app/routes/user.py
+++ app/routes/user.py
@@ -2,6 +2,8 @@ from app import app, db
 from datetime import date
 from flask import Flask, request, jsonify
 from app.models.user import User
+from app.utils.authentication import createToken, hashPassword, validatePassword
+import jwt
 
 
 @app.route('/baguette/api/v1.0/users', methods=['GET'])
@@ -24,31 +26,89 @@ def get_user(user_id):
         return(str(e))
 
 
-@app.route('/baguette/api/v1.0/users', methods=['POST'])
+@app.route('/baguette/api/v1.0/signup', methods=['POST'])
 def create_user():
 
-    userInfo = request.get_json()
-    y, m, d = userInfo['date_of_birth'].split('T')[0].split('-')
+    userRequest = request.get_json()
+    y, m, d = userRequest['date_of_birth'].split('T')[0].split('-')
+    hashedPassword = hashPassword(userRequest['password'])
 
     try:
         user = User(
-            username=userInfo['username'],
-            password=userInfo['password'],
-            email=userInfo['email'],
-            first_name=userInfo['first_name'],
-            last_name=userInfo['last_name'],
+            username=userRequest['username'],
+            password=hashedPassword,
+            email=userRequest['email'],
+            first_name=userRequest['first_name'],
+            last_name=userRequest['last_name'],
             date_of_birth=date(int(y), int(m), int(d))
         )
         db.session.add(user)
         db.session.commit()
-        print("User {} added user id={}".format(username, user.id))
-        return jsonify({'user': user.serialize()}), 201
+        print("User {} added user id={}".format(user.username, user.id))
+
+        userData = {
+            "username": user.username,
+            "id": str(user.id),
+            "email": user.email,
+            "firstName": user.first_name,
+            "lastName": user.last_name
+        }
+        token = createToken(userData)
+        decodedToken = jwt.decode(
+            token, app.config["SECRET_KEY"], algorithms="HS256")
+        expiryTime = decodedToken["exp"]
+
+        return jsonify({
+            "token": token,
+            "expiryTime": expiryTime,
+            "userData": userData
+        }), 201
+
     except Exception as e:
         db.session.rollback()
         return(str(e))
 
 
-@app.route('/baguette/api/v1.0/users/<user_id>', methods=['PUT'])
+@ app.route('/baguette/api/v1.0/auth', methods=['POST'])
+def auth_user():
+    userRequest = request.get_json()
+    email = userRequest['email']
+    password = userRequest['password']
+
+    try:
+        user = User.query.filter_by(email=email).first()
+        if user is None:
+            return "User doesn't exist"
+
+        isPasswordValid = validatePassword(password, user.password)
+
+        if isPasswordValid is False:
+            return "Incorrect password"
+
+        userData = {
+            "username": user.username,
+            "id": str(user.id),
+            "email": user.email,
+            "firstName": user.first_name,
+            "lastName": user.last_name
+        }
+        token = createToken(userData)
+        decodedToken = jwt.decode(
+            token, app.config["SECRET_KEY"], algorithms="HS256")
+        expiryTime = decodedToken["exp"]
+
+        return jsonify({
+            "token": token,
+            "expiryTime": expiryTime,
+            "userData": userData
+        }), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return(str(e))
+
+
+@ app.route('/baguette/api/v1.0/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
     try:
         user = User.query.filter_by(id=user_id).first()
@@ -84,13 +144,13 @@ def update_user(user_id):
         return(str(e))
 
 
-@app.route('/baguette/api/v1.0/users/<user_id>', methods=['DELETE'])
+@ app.route('/baguette/api/v1.0/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     try:
         user = User.query.filter_by(id=user_id).first()
         db.session.delete(user)
         db.session.commit()
-        print("User {} deleted user id={}".format(username, user.id))
+        print("User {} deleted user id={}".format(user.username, user.id))
     except Exception as e:
         db.session.rollback()
         return(str(e))
