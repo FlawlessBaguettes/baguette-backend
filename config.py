acc91dceec17e0e903d87046be44731788491051
Raghav Goswami <raghav.goswami576@gmail.com>
2021-04-08 11:22:42 -0400

    encrypt secrets

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import json
 import datetime
 import os
 import vimeo
@@ -5,10 +6,24 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+
+    # To access secrets stored in secrets.ejson, you must first decrypt them.
+    # Decrypt the secrets.ejson by following the steps below:
+    #   1. Paste your private key in the file: `/opt/ejson/keys/<public key>`
+    #   2. Run the command: `ejson decrypt secrets.ejson` which outputs secrets.ejson in plaintext to stdout
+    #   3. Copy the plaintext from stdout and paste it in secrets.ejson, overwriting the ciphertext
+
+    # IMPORTANT: To prevent secrets leaking, re-encrypt secrets.ejson before committing to version control.
+    # Run `ejson encrypt secrets.ejson` to encrypt secrets.ejson
+    # (secrets.ejson is included in .gitignore as an extra precaution)
+    # See https://github.com/Shopify/ejson for detailed instructions.
+
+    with open('secrets.ejson') as ejson_file:
+        secretsData = json.load(ejson_file)
+
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
 
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -18,12 +33,15 @@ class Config(object):
     UPLOAD_PATH = 'uploads'
 
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(weeks=52)
+
+    SECRET_KEY = secretsData["SECRET_KEY"]
     JWT_SECRET_KEY = SECRET_KEY
 
+    VIMEO_CLIENT = secretsData["VIMEO_CLIENT"]
     VIMEO_CLIENT = vimeo.VimeoClient(
-        token='29b2d3f5fcbbf63d8f966f7c85973fe5',
-        key='cd8615a35359c7d8bd60bb9766a9e9a80aa1ef01',
-        secret='Xy1iTA658MNPtM8AjOezNafd33kaQx7s/Lw3rO5u8Swh6+xH1nsqCnO5eTt093n0y20kJc0mo/jEgWM/MxKjWH1wNc4zu/7VSyfbR55C4mWaEQvqvDvGm7Ay4EuVK/A4'
+        token=VIMEO_CLIENT.token,
+        key=VIMEO_CLIENT.key,
+        secret=VIMEO_CLIENT.secret
     )
 
 
